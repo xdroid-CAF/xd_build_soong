f0b71ebba5b1747b3bd20741f8fc3bbb5f495d7a
Pig <pig.priv@gmail.com>
2020-10-15 02:50:07 +0800

    soong: Drop Xdroid soong product variables
     * This reverts commits c59e356, ce8c7b4, f6807aa in preparation
       for using Soong config variables as per Google's docs.
    
    Change-Id: Ice01dcd8da10e3e2c38394acd9fe9a9ccb1d9784

--- android/variable.go
+++ android/variable.go
@@ -19,9 +19,6 @@ import (
 	"reflect"
 	"runtime"
 	"strings"
-
-	"xdroid/soong/android"
-
 	"github.com/google/blueprint/proptools"
 )
 
@@ -162,8 +159,6 @@ type variableProperties struct {
 			Cflags []string
 		}
 
-		// include Xdroid variables
-		Xdroid android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -385,7 +380,6 @@ type productVariables struct {
 
 	PrebuiltHiddenApiDir *string `json:",omitempty"`
 
-	Xdroid android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
@@ -454,23 +448,15 @@ func VariableMutator(mctx BottomUpMutatorContext) {
 	}
 
 	variableValues := reflect.ValueOf(a.variableProperties).Elem().FieldByName("Product_variables")
-	valStruct := reflect.ValueOf(mctx.Config().productVariables)
 
-	doVariableMutation(mctx, a, variableValues, valStruct)
-}
-
-func doVariableMutation(mctx BottomUpMutatorContext, a *ModuleBase, variableValues reflect.Value, valStruct reflect.Value) {
 	for i := 0; i < variableValues.NumField(); i++ {
 		variableValue := variableValues.Field(i)
 		name := variableValues.Type().Field(i).Name
 		property := "product_variables." + proptools.PropertyNameForField(name)
 
 		// Check that the variable was set for the product
-		val := valStruct.FieldByName(name)
-		if val.IsValid() && val.Kind() == reflect.Struct {
-			doVariableMutation(mctx, a, variableValue, val)
-			continue
-		} else if !val.IsValid() || val.Kind() != reflect.Ptr || val.IsNil() {
+		val := reflect.ValueOf(mctx.Config().productVariables).FieldByName(name)
+		if !val.IsValid() || val.Kind() != reflect.Ptr || val.IsNil() {
 			continue
 		}
 
@@ -642,10 +628,6 @@ func createVariableProperties(moduleTypeProps []interface{}, productVariables in
 func createVariablePropertiesType(moduleTypeProps []interface{}, productVariables interface{}) reflect.Type {
 	typ, _ := proptools.FilterPropertyStruct(reflect.TypeOf(productVariables),
 		func(field reflect.StructField, prefix string) (bool, reflect.StructField) {
-			if strings.HasPrefix(prefix, "Product_variables.Xdroid") {
-				// Convert Product_variables.Xdroid.Foo to Xdroid.Foo
-				_, prefix = splitPrefix(prefix)
-			}
 
 			// Filter function, returns true if the field should be in the resulting struct
 			if prefix == "" {
